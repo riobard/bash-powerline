43e67c6e22b414582a4a07261536c13e8f42f96d
Hexcles Ma <bob1211@gmail.com>
2014-08-08 21:20:41 +0800

    Add Python virtualenv support

--- bash-powerline.sh
+++ bash-powerline.sh
@@ -89,6 +89,21 @@ __powerline() {
         printf " $GIT_BRANCH_SYMBOL$branch$marks "
     }
 
+    __virtualenv() {
+        # Copied from Python virtualenv's activate.sh script.
+        # https://github.com/pypa/virtualenv/blob/a9b4e673559a5beb24bac1a8fb81446dd84ec6ed/virtualenv_embedded/activate.sh#L62
+        # License: MIT
+        if [ "x$VIRTUAL_ENV" != "x" ]; then
+            if [ "`basename \"$VIRTUAL_ENV\"`" == "__" ]; then
+                # special case for Aspen magic directories
+                # see http://www.zetadev.com/software/aspen/
+                printf "[`basename \`dirname \"$VIRTUAL_ENV\"\``]"
+            else
+                printf "(`basename \"$VIRTUAL_ENV\"`)"
+            fi
+        fi
+    }
+
     ps1() {
         # Check the exit code of the previous command and display different
         # colors in the prompt accordingly. 
@@ -98,7 +113,9 @@ __powerline() {
             local BG_EXIT="$BG_RED"
         fi
 
-        PS1="$BG_BASE1$FG_BASE3 \w $RESET"
+        PS1=""
+        PS1+="$BG_BASE0$FG_BASE3$(__virtualenv)$RESET"
+        PS1+="$BG_BASE1$FG_BASE3 \w $RESET"
         PS1+="$BG_BLUE$FG_BASE3$(__git_info)$RESET"
         PS1+="$BG_EXIT$FG_BASE3 $PS_SYMBOL $RESET "
     }
