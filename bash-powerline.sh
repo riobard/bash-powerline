67ac8546b4f0f049218be5e3a38ae65662549144
Riobard <me@riobard.com>
2017-09-24 23:43:33 +0800

    Simplified colorscheme and got rid of `tput`
    Solarized colorscheme is no longer required (but still recommended).
    Use color esacpe sequence directly instead of relying on `tput` so we
    have one fewer external dependency.

--- bash-powerline.sh
+++ bash-powerline.sh
@@ -4,99 +4,21 @@
 #POWERLINE_GIT=0
 
 __powerline() {
-    if ! hash tput 2>/dev/null; then
-        >&2 echo "tput missing, install ncurses to use bash-powerline.sh"
-        return
-    fi
-
     # Unicode symbols
     readonly PS_SYMBOL_DARWIN=''
     readonly PS_SYMBOL_LINUX='$'
     readonly PS_SYMBOL_OTHER='%'
-    readonly GIT_BRANCH_SYMBOL='⑂ '
+    readonly GIT_BRANCH_SYMBOL='⑂'
     readonly GIT_BRANCH_CHANGED_SYMBOL='+'
     readonly GIT_NEED_PUSH_SYMBOL='⇡'
     readonly GIT_NEED_PULL_SYMBOL='⇣'
 
-    # Solarized colorscheme
-    if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
-        readonly FG_BASE03="\[$(tput setaf 234)\]"
-        readonly FG_BASE02="\[$(tput setaf 235)\]"
-        readonly FG_BASE01="\[$(tput setaf 240)\]"
-        readonly FG_BASE00="\[$(tput setaf 241)\]"
-        readonly FG_BASE0="\[$(tput setaf 244)\]"
-        readonly FG_BASE1="\[$(tput setaf 245)\]"
-        readonly FG_BASE2="\[$(tput setaf 254)\]"
-        readonly FG_BASE3="\[$(tput setaf 230)\]"
-
-        readonly BG_BASE03="\[$(tput setab 234)\]"
-        readonly BG_BASE02="\[$(tput setab 235)\]"
-        readonly BG_BASE01="\[$(tput setab 240)\]"
-        readonly BG_BASE00="\[$(tput setab 241)\]"
-        readonly BG_BASE0="\[$(tput setab 244)\]"
-        readonly BG_BASE1="\[$(tput setab 245)\]"
-        readonly BG_BASE2="\[$(tput setab 254)\]"
-        readonly BG_BASE3="\[$(tput setab 230)\]"
-
-        readonly FG_YELLOW="\[$(tput setaf 136)\]"
-        readonly FG_ORANGE="\[$(tput setaf 166)\]"
-        readonly FG_RED="\[$(tput setaf 160)\]"
-        readonly FG_MAGENTA="\[$(tput setaf 125)\]"
-        readonly FG_VIOLET="\[$(tput setaf 61)\]"
-        readonly FG_BLUE="\[$(tput setaf 33)\]"
-        readonly FG_CYAN="\[$(tput setaf 37)\]"
-        readonly FG_GREEN="\[$(tput setaf 64)\]"
-
-        readonly BG_YELLOW="\[$(tput setab 136)\]"
-        readonly BG_ORANGE="\[$(tput setab 166)\]"
-        readonly BG_RED="\[$(tput setab 160)\]"
-        readonly BG_MAGENTA="\[$(tput setab 125)\]"
-        readonly BG_VIOLET="\[$(tput setab 61)\]"
-        readonly BG_BLUE="\[$(tput setab 33)\]"
-        readonly BG_CYAN="\[$(tput setab 37)\]"
-        readonly BG_GREEN="\[$(tput setab 64)\]"
-     else
-        readonly FG_BASE03="\[$(tput setaf 8)\]"
-        readonly FG_BASE02="\[$(tput setaf 0)\]"
-        readonly FG_BASE01="\[$(tput setaf 10)\]"
-        readonly FG_BASE00="\[$(tput setaf 11)\]"
-        readonly FG_BASE0="\[$(tput setaf 12)\]"
-        readonly FG_BASE1="\[$(tput setaf 14)\]"
-        readonly FG_BASE2="\[$(tput setaf 7)\]"
-        readonly FG_BASE3="\[$(tput setaf 15)\]"
-
-        readonly BG_BASE03="\[$(tput setab 8)\]"
-        readonly BG_BASE02="\[$(tput setab 0)\]"
-        readonly BG_BASE01="\[$(tput setab 10)\]"
-        readonly BG_BASE00="\[$(tput setab 11)\]"
-        readonly BG_BASE0="\[$(tput setab 12)\]"
-        readonly BG_BASE1="\[$(tput setab 14)\]"
-        readonly BG_BASE2="\[$(tput setab 7)\]"
-        readonly BG_BASE3="\[$(tput setab 15)\]"
-
-        readonly FG_YELLOW="\[$(tput setaf 3)\]"
-        readonly FG_ORANGE="\[$(tput setaf 9)\]"
-        readonly FG_RED="\[$(tput setaf 1)\]"
-        readonly FG_MAGENTA="\[$(tput setaf 5)\]"
-        readonly FG_VIOLET="\[$(tput setaf 13)\]"
-        readonly FG_BLUE="\[$(tput setaf 4)\]"
-        readonly FG_CYAN="\[$(tput setaf 6)\]"
-        readonly FG_GREEN="\[$(tput setaf 2)\]"
-
-        readonly BG_YELLOW="\[$(tput setab 3)\]"
-        readonly BG_ORANGE="\[$(tput setab 9)\]"
-        readonly BG_RED="\[$(tput setab 1)\]"
-        readonly BG_MAGENTA="\[$(tput setab 5)\]"
-        readonly BG_VIOLET="\[$(tput setab 13)\]"
-        readonly BG_BLUE="\[$(tput setab 4)\]"
-        readonly BG_CYAN="\[$(tput setab 6)\]"
-        readonly BG_GREEN="\[$(tput setab 2)\]"
-    fi
-
-    readonly DIM="\[$(tput dim)\]"
-    readonly REVERSE="\[$(tput rev)\]"
-    readonly RESET="\[$(tput sgr0)\]"
-    readonly BOLD="\[$(tput bold)\]"
+    # Colorscheme
+    readonly RESET='\[\033[m\]'
+    readonly COLOR_CWD='\[\033[0;34m\]' # blue
+    readonly COLOR_GIT='\[\033[0;36m\]' # cyan
+    readonly COLOR_SYMBOL_SUCCESS='\[\033[0;32m\]' # green
+    readonly COLOR_SYMBOL_FAILURE='\[\033[0;31m\]' # red
 
     if [[ -z "$PS_SYMBOL" ]]; then
       case "$(uname)" in
@@ -136,19 +58,19 @@ __powerline() {
         done < <($git_eng status --porcelain --branch)  # note the space between the two <
 
         # print the git branch segment without a trailing newline
-        printf " $GIT_BRANCH_SYMBOL$branch$marks "
+        printf " $GIT_BRANCH_SYMBOL$branch$marks"
     }
 
     ps1() {
         # Check the exit code of the previous command and display different
         # colors in the prompt accordingly. 
         if [ $? -eq 0 ]; then
-            local BG_EXIT="$BG_GREEN"
+            local symbol="$COLOR_SYMBOL_SUCCESS $PS_SYMBOL $RESET"
         else
-            local BG_EXIT="$BG_RED"
+            local symbol="$COLOR_SYMBOL_FAILURE $PS_SYMBOL $RESET"
         fi
 
-        PS1="$BG_BASE1$FG_BASE3 \w $RESET"
+        local cwd="$COLOR_CWD\w$RESET"
         # Bash by default expands the content of PS1 unless promptvars is disabled.
         # We must use another layer of reference to prevent expanding any user
         # provided strings, which would cause security issues.
@@ -156,12 +78,13 @@ __powerline() {
         # Related fix in git-bash: https://github.com/git/git/blob/9d77b0405ce6b471cb5ce3a904368fc25e55643d/contrib/completion/git-prompt.sh#L324
         if shopt -q promptvars; then
             __powerline_git_info="$(__git_info)"
-            PS1+="$BG_BLUE$FG_BASE3\${__powerline_git_info}$RESET"
+            local git="$COLOR_GIT\${__powerline_git_info}$RESET"
         else
             # promptvars is disabled. Avoid creating unnecessary env var.
-            PS1+="$BG_BLUE$FG_BASE3$(__git_info)$RESET"
+            local git="$COLOR_GIT$(__git_info)$RESET"
         fi
-        PS1+="$BG_EXIT$FG_BASE3 $PS_SYMBOL $RESET "
+
+        PS1="$cwd$git$symbol"
     }
 
     PROMPT_COMMAND=ps1
